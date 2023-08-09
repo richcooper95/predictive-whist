9a286a5a5ab94e5accafd26be1a304c7fb186055
Rich Barton-Cooper <rich@duffel.com>
2023-08-09 23:47:14 +0100

    Try and fix 500s by using os.path.join for STATIC_ROOT

--- predictive_whist/settings.py
+++ predictive_whist/settings.py
@@ -181,7 +181,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "static/"
 
 STORAGES = {
